376c951384385204fd9b115fc88737568bfffb83
Didi Walakutty <91764214+DidiWalakutty@users.noreply.github.com>
2023-07-26 16:33:47 +0200

    Update b/o split-function

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 18:27:25 by diwalaku      #+#    #+#                 */
-/*   Updated: 2023/07/24 17:17:43 by diwalaku      ########   odam.nl         */
+/*   Updated: 2023/07/26 16:33:17 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@
 int		ft_isdigit(int d);
 size_t	ft_strlen(const char *s);
 void	ft_putchar(char c);
-void	ft_putendl(char *s);
-void	ft_putstr(char *str);
 void	ft_putstr_fd(char *s, int fd);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	**ft_split(char const *str, char c);
+void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+char	**ft_split(char const *str, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
